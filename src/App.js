c65365caf6830b64ab1a74789de2d6b57334eab2
Mike Chi <mikechi@macbook-pro-4.lan>
2020-04-02 09:32:36 -0700

    installing packages and removing unnecessary pieces

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+// TODO: add created components here
+import SignupForm from "./components/signupform/SignupForm";
 
-function App() {
+const PageOne = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Page One
+      <Link to="/pagetwo">Hey there</Link>
     </div>
   );
-}
+};
+const PageTwo = () => {
+  return (
+    <div>
+      Page Two
+      <Link to="/">Click Me</Link>
+    </div>
+  );
+};
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Route path="/" exact component={SignupForm} />
+        <Route path="/login" component={PageTwo} />
+        {/* <Route path="/signup" component={Signup}/> */}
+        {/* <Route path="/edit" component={EditTask}/> */}
+        {/* <Route path="/add" component={AddTask}/> */}
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
