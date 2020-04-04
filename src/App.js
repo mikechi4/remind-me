dd90b437d960691f0a83429aed223cb7182b1149
Mike Chi <mikechi@macbook-pro-4.lan>
2020-04-04 13:31:51 -0700

    adding home component

--- src/App.js
+++ src/App.js
@@ -3,30 +3,15 @@ import { BrowserRouter, Route, Link } from "react-router-dom";
 
 import SignupForm from "./components/signupform/SignupForm";
 import Login from './components/login/Login';
+import Home from './components/home/Home';
 
-const PageOne = () => {
-  return (
-    <div>
-      Page One
-      <Link to="/pagetwo">Hey there</Link>
-    </div>
-  );
-};
-const PageTwo = () => {
-  return (
-    <div>
-      Page Two
-      <Link to="/">Click Me</Link>
-    </div>
-  );
-};
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Route path="/" exact component={SignupForm} />
+        <Route path="/signup" component={SignupForm} />
         <Route path="/login" component={Login} />
-        {/* <Route path="/signup" component={Signup}/> */}
+        <Route path="/home" exact component={Home} />
         {/* <Route path="/edit" component={EditTask}/> */}
         {/* <Route path="/add" component={AddTask}/> */}
       </BrowserRouter>
---
+++ src/components/home/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Axios from 'axios';
+
+import './Home.scss';
+
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>Home</div>
+        )
+    }
+}
+
+export default Home;
