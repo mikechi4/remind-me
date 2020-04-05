7cc8ef5125612f5b21c718003a9959be019eb7c8
Mike Chi <mikechi@macbook-pro-4.lan>
2020-04-05 15:17:21 -0700

    creating separate function for getting reminders. we trigger that call when a new reminder is saved and when the component mounts

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,22 +1,53 @@
 import React from 'react';
 import Axios from 'axios';
-
+import { ListGroup } from 'react-bootstrap';
 import './Home.scss';
 
 import ReminderModal from '../reminder-modal/ReminderModal';
+import ReminderItem from '../reminder-item/ReminderItem';
+
 
 class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            showModal: false
+            showModal: false,
+            reminderList: [],
+            savedNewReminder: false
         }
     }
 
-    toggleModalState = () => {
+    getReminders = () => {
+        Axios.get("/api/reminders").then((response) => {
+            const data = response.data;
+            this.setState({ reminderList: data });
+        })
+    }
+
+    renderReminderList = (savedReminders) => {
+        return savedReminders.map(item => {
+            return (
+                <ReminderItem
+                    reminder={item.reminder}
+                    dueDate={item.dueDate}
+                    key={item._id}
+                />
+            );
+        });
+    }
+
+    componentDidMount = () => {
+        this.getReminders();
+    }
+
+    toggleModalState = (didSaveToDb) => {
         this.setState({
             showModal: !this.state.showModal
         })
+        // this flag is to see if the user did a save. That way, we can make an API call to re-render most up to date list
+        if (didSaveToDb) {
+            this.getReminders()
+        }
     }
 
     render() {
@@ -29,6 +60,11 @@ class Home extends React.Component {
                 <div className="list-header">
                     <h2>Reminder(s)</h2>
                     <button className="btn btn-primary" onClick={this.toggleModalState}>Create Reminder</button>
+                </div>
+                <div className="reminder-list">
+                    <ListGroup>
+                        {this.state.reminderList.length <= 0 ? "Add reminders" : this.renderReminderList(this.state.reminderList)}
+                    </ListGroup>
                 </div>
             </div>
         )
