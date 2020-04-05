90b02cb7d2a95a567eaef05bc306c57fcc4b21ff
Mike Chi <mikechi@macbook-pro-4.lan>
2020-04-05 15:39:11 -0700

    addign styling

--- src/components/reminder-item/ReminderItem.js
+++ src/components/reminder-item/ReminderItem.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import "./ReminderItem.scss";
-import { ListGroup } from 'react-bootstrap';
-// import { ArrowRight } from 'react-bootstrap-icons';
+import { ListGroup, Button } from 'react-bootstrap';
+import { Trash, PencilSquare, ThreeDotsVertical } from 'react-bootstrap-icons';
+
+import './ReminderItem.scss';
 const ReminderItem = (props) => {
 
     return (
         <ListGroup.Item className="reminder-item-container">
-            <div className="reminder-item-name">{props.reminder}</div>
-            <div className="reminder-item-date">{props.dueDate}</div>
+            <div className="reminder-item">
+                <div className="reminder-item-name">{props.reminder}</div>
+                <div className="reminder-item-date">{props.dueDate}</div>
+            </div>
+            <div className="action-buttons">
+                <Button variant="danger"><Trash /></Button>
+                <ThreeDotsVertical />
+                <Button variant="primary"><PencilSquare /></Button>
+            </div>
         </ListGroup.Item>
     )
 
