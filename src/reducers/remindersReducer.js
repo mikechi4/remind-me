82826977d043deb26c158ba1a0aed590ff656a34
Mike Chi <mikechi@macbook-pro-4.lan>
2020-04-12 23:37:31 -0700

    adding delete logic

--- src/reducers/remindersReducer.js
+++ src/reducers/remindersReducer.js
@@ -1,7 +1,20 @@
+const findIndexById = (list, id) => {
+    return list.findIndex((reminder) => {
+        return reminder._id === id
+    });
+}
+const removeDeletedReminder = (prevState, deletedReminderId) => {
+    const updatedReminderList = prevState.slice();
+    let index = findIndexById(updatedReminderList, deletedReminderId);
+    return updatedReminderList.splice(index, 1);
+}
+
 export default (state = [], action) => {
     switch (action.type) {
         case "GET_REMINDERS":
             return action.payload;
+        case "DELETE_REMINDER":
+            return removeDeletedReminder(state, action.payload);
         default:
             return state
     }
