15177d93d91fc3bef1a6f2f5595ee1130b7063b9
Mike Chi <mikechi@macbook-pro-4.lan>
2020-04-12 18:20:38 -0700

    adding provider and store to index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
 import App from "./App";
+import reducers from './reducers';
+
 import { BrowserRouter, Route } from "react-router-dom";
 
+//hookup middleware to store
+const store = createStore(reducers, applyMiddleware(thunk));
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById("root")
 );
